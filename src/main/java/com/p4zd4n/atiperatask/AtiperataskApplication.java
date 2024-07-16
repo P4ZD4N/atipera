0a200caf657a5bbf7ddf9ab72f20386b02949bf0
Wiktor <wiko700@gmail.com>
2024-07-16 23:57:18 +0200

    init: add initial files

---
+++ src/main/java/com/p4zd4n/atiperatask/AtiperataskApplication.java
@@ -0,0 +1,13 @@
+package com.p4zd4n.atiperatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperataskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperataskApplication.class, args);
+	}
+
+}
