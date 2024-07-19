ece9076fbc07a54429692b078a289cdcffec1e97
Wiktor <wiko700@gmail.com>
2024-07-19 17:21:26 +0200

    feat: add more tests of GitHubService class

--- src/test/java/com/p4zd4n/atiperatask/GitHubServiceTest.java
+++ src/test/java/com/p4zd4n/atiperatask/GitHubServiceTest.java
@@ -67,4 +67,43 @@ public class GitHubServiceTest {
 
         assertThrows(UserNotFoundException.class, () -> gitHubService.getRepositories(username));
     }
+
+    @Test
+    void getRepositories_emptyRepositories_returnsEmptyList() {
+
+
+        when(restTemplate.getForObject(anyString(), eq(Repository[].class)))
+                .thenReturn(new Repository[0]);
+
+        List<RepositoryResponse> repositoryResponses = gitHubService.getRepositories(username);
+
+        assertNotNull(repositoryResponses);
+        assertTrue(repositoryResponses.isEmpty());
+        verify(restTemplate, times(1)).getForObject(anyString(), eq(Repository[].class));
+    }
+
+    @Test
+    void getRepositories_withDifferentBranchNames_returnsCorrectBranches() {
+
+        Branch[] branches = {
+                new Branch("main", new LastCommit("sha5")),
+                new Branch("feature-branch", new LastCommit("sha7"))
+        };
+
+        Repository[] repositories = {
+                Repository.builder().name("repo1").owner(owner).build()
+        };
+
+        when(restTemplate.getForObject(anyString(), eq(Repository[].class))).thenReturn(repositories);
+        when(restTemplate.getForObject(anyString(), eq(Branch[].class))).thenReturn(branches);
+        when(restTemplate.getForObject(anyString(), eq(LastCommit.class))).thenReturn(new LastCommit("sha5"));
+
+        List<RepositoryResponse> repositoryResponses = gitHubService.getRepositories(username);
+
+        assertNotNull(repositoryResponses);
+        assertEquals(1, repositoryResponses.size());
+        assertEquals(2, repositoryResponses.getFirst().branches().size());
+        assertEquals("main", repositoryResponses.getFirst().branches().getFirst().name());
+        assertEquals("feature-branch", repositoryResponses.getFirst().branches().get(1).name());
+    }
 }
