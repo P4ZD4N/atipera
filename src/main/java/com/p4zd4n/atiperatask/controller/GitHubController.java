bf5fe287c2212cc005c16cc2f6fc13703fd76556
Wiktor <wiko700@gmail.com>
2024-07-19 14:44:39 +0200

    cleanup: remove unused imports

--- src/main/java/com/p4zd4n/atiperatask/controller/GitHubController.java
+++ src/main/java/com/p4zd4n/atiperatask/controller/GitHubController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 @AllArgsConstructor
