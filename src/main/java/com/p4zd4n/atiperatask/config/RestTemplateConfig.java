d9b81d1cc0a1424a5837c128697410386d86a21c
Wiktor <wiko700@gmail.com>
2024-07-19 13:58:26 +0200

    feat: add optional authorization header to requests

--- src/main/java/com/p4zd4n/atiperatask/config/RestTemplateConfig.java
+++ src/main/java/com/p4zd4n/atiperatask/config/RestTemplateConfig.java
@@ -1,14 +1,48 @@
 package com.p4zd4n.atiperatask.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Collections;
 
 @Configuration
 public class RestTemplateConfig {
 
+    @Value("${github.auth.token}")
+    private String authToken;
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(new RestTemplateHeaderModifier(authToken)));
+        return restTemplate;
+    }
+
+    public static class RestTemplateHeaderModifier implements ClientHttpRequestInterceptor {
+
+        private final String authToken;
+
+        public RestTemplateHeaderModifier(String authToken) {
+            this.authToken = authToken;
+        }
+
+        @Override
+        public ClientHttpResponse intercept(
+                HttpRequest request,
+                byte[] body,
+                ClientHttpRequestExecution execution
+        ) throws IOException {
+            if (authToken != null && !authToken.isEmpty()) {
+                request.getHeaders().set("Authorization", "Bearer " + authToken);
+            }
+            return execution.execute(request, body);
+        }
     }
 }
