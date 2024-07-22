8d09a914ccc144933d4074eff40b5a33707918e5
Wiktor <wiko700@gmail.com>
2024-07-22 14:29:39 +0200

    refactor: adapt test to previous refactor in GitHubService class

--- src/test/java/com/p4zd4n/atiperatask/service/GitHubServiceTest.java
+++ src/test/java/com/p4zd4n/atiperatask/service/GitHubServiceTest.java
@@ -6,14 +6,20 @@ import com.p4zd4n.atiperatask.model.LastCommit;
 import com.p4zd4n.atiperatask.model.Owner;
 import com.p4zd4n.atiperatask.model.Repository;
 import com.p4zd4n.atiperatask.response.RepositoryResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,17 +28,26 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class GitHubServiceTest {
 
     @Mock
     private RestTemplate restTemplate;
 
-    @InjectMocks
+    @Autowired
     private GitHubService gitHubService;
+
+    @Value("${github.api.url}")
+    private String apiUrl;
 
     private final String username = "testUser";
     private final Owner owner = new Owner(username);
+
+    @BeforeEach
+    void setUp() {
+        gitHubService = new GitHubService(restTemplate, apiUrl);
+    }
 
     @Test
     void getRepositories_userExists_returnsRepositories() {
@@ -47,21 +62,21 @@ public class GitHubServiceTest {
                 Repository.builder().name("repo2").owner(owner).branches(Arrays.asList(branches)).build()
         };
 
-        when(restTemplate.getForObject(anyString(), eq(Repository[].class))).thenReturn(repositories);
-        when(restTemplate.getForObject(anyString(), eq(Branch[].class))).thenReturn(branches);
-        when(restTemplate.getForObject(anyString(), eq(LastCommit.class))).thenReturn(new LastCommit("sha1"));
+        when(restTemplate.getForObject(any(URI.class), eq(Repository[].class))).thenReturn(repositories);
+        when(restTemplate.getForObject(any(URI.class), eq(Branch[].class))).thenReturn(branches);
+        when(restTemplate.getForObject(any(URI.class), eq(LastCommit.class))).thenReturn(new LastCommit("sha1"));
 
         List<RepositoryResponse> repositoryResponses = gitHubService.getRepositories(username);
 
         assertNotNull(repositoryResponses);
         assertEquals(2, repositoryResponses.size());
-        verify(restTemplate, times(1)).getForObject(anyString(), eq(Repository[].class));
+        verify(restTemplate, times(1)).getForObject(any(URI.class), eq(Repository[].class));
     }
 
     @Test
     void getRepositories_userDoesNotExist_throwsUserNotFoundException() {
 
-        when(restTemplate.getForObject(anyString(), eq(Repository[].class)))
+        when(restTemplate.getForObject(any(URI.class), eq(Repository[].class)))
                 .thenThrow(HttpClientErrorException.NotFound.class);
 
         assertThrows(UserNotFoundException.class, () -> gitHubService.getRepositories(username));
@@ -71,14 +86,14 @@ public class GitHubServiceTest {
     void getRepositories_emptyRepositories_returnsEmptyList() {
 
 
-        when(restTemplate.getForObject(anyString(), eq(Repository[].class)))
+        when(restTemplate.getForObject(any(URI.class), eq(Repository[].class)))
                 .thenReturn(new Repository[0]);
 
         List<RepositoryResponse> repositoryResponses = gitHubService.getRepositories(username);
 
         assertNotNull(repositoryResponses);
         assertTrue(repositoryResponses.isEmpty());
-        verify(restTemplate, times(1)).getForObject(anyString(), eq(Repository[].class));
+        verify(restTemplate, times(1)).getForObject(any(URI.class), eq(Repository[].class));
     }
 
     @Test
@@ -93,9 +108,9 @@ public class GitHubServiceTest {
                 Repository.builder().name("repo1").owner(owner).build()
         };
 
-        when(restTemplate.getForObject(anyString(), eq(Repository[].class))).thenReturn(repositories);
-        when(restTemplate.getForObject(anyString(), eq(Branch[].class))).thenReturn(branches);
-        when(restTemplate.getForObject(anyString(), eq(LastCommit.class))).thenReturn(new LastCommit("sha5"));
+        when(restTemplate.getForObject(any(URI.class), eq(Repository[].class))).thenReturn(repositories);
+        when(restTemplate.getForObject(any(URI.class), eq(Branch[].class))).thenReturn(branches);
+        when(restTemplate.getForObject(any(URI.class), eq(LastCommit.class))).thenReturn(new LastCommit("sha5"));
 
         List<RepositoryResponse> repositoryResponses = gitHubService.getRepositories(username);
 
