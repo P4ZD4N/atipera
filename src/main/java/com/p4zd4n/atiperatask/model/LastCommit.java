0559f15897fec264de0775f1aa451fe9d6f4180b
Wiktor <wiko700@gmail.com>
2024-07-22 19:29:41 +0200

    refactor: remove useless @JsonProperty annotation

--- src/main/java/com/p4zd4n/atiperatask/model/LastCommit.java
+++ src/main/java/com/p4zd4n/atiperatask/model/LastCommit.java
@@ -1,7 +1,5 @@
 package com.p4zd4n.atiperatask.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record LastCommit(
-        @JsonProperty("sha") String sha
+        String sha
 ) {}
